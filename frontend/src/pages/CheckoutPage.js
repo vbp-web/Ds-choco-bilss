740ed65684640a11bae9b38830327e188478cabc
PRAJAPATI VANSH <prajapativansh512@gmail.com>
2025-10-16 10:57:38 +0530

    Fix React DOM compatibility for Vercel deployment

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useCart } from '../context/CartContext';
-import axios from 'axios';
 import toast from 'react-hot-toast';
 import { FaUser, FaPhone, FaEnvelope, FaMapMarkerAlt, FaLock } from 'react-icons/fa';
 
